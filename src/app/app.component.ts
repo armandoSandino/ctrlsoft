b0c516795bc88d9744079c21271c6100dd2c9d26
Armando Sandino <jsandino@arkkosoft.net>
2022-01-21 11:03:44 -0600

    test; test business logic implementation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { StatementAccountService } from './app/core/services/statement-account.service';
 import { ResponseStatementAccount, StatementAccount } from './app/shared/models/statement-account.model';
 import { FormBuilder, FormControl, FormGroup, Validators} from '@angular/forms';
+import date from 'date-and-time';
 
 export interface StatementElement {
   id?:number;
@@ -11,6 +12,10 @@ export interface StatementElement {
   paymentDate?:string;
   amount?:number;
   cTipo?:string;
+  isLess?:boolean; // es menor a fecha actual
+  isGreater?: boolean; // es mayor y menor a 3 meses
+  isValidDueDate?:boolean; //
+  isGreaterThanThreeMonths?: boolean; // es mayor a 3 meses
 }
 
 
@@ -43,10 +48,10 @@ export class AppComponent {
 
   loadData(term?: string ): void {
     this.statementAccountService.getStatementAccount( term ).subscribe( (response: ResponseStatementAccount ) => {
-
+    
       this.statementAccount = response?.response;
       console.error( this.statementAccount );
-      // if ( this.statementAccount?.PagosRealizados?.PagosRealizados.length > 0 ) {
+      
       if (  this.statementAccount?.opcNombre !== '' && this.statementAccount?.opcNombre !== null ) {
         this.statementAccount?.PagosRealizados?.PagosRealizados.forEach( (value , index ) => {
           this?.ELEMENT_DATA.push({
@@ -61,19 +66,93 @@ export class AppComponent {
         });
         this.paymentMade = this.ELEMENT_DATA;
 
-        this.statementAccount?.PagosPorRealizar?.PagosPorRealizar.forEach( (value , index ) => {
-          this?.ELEMENT_DATA_BE_MADE.push({
-            id: value?.id,
-            nContribution: value?.iNoAport,
-            concept: value?.cConcepto,
-            dueDate: value?.cVencimiento,
-            paymentDate: value?.cFechaPago,
-            amount: value?.deMonto,
-            cTipo: value?.cTipo
-          });
+        this.statementAccount?.PagosPorRealizar?.PagosPorRealizar.every( (value , index, array ) => {
+          
+          // #7AF0D0  Si da error al convertir
+          try {
+            
+            //  date.isValid('29-02-2015', 'DD-MM-YYYY');  
+
+            /*
+            let now = new Date();
+            let cVencimiento = new Date( String(value?.cVencimiento.toString()).trim() );
+            console.error('Complete object ',  value );
+            console.error('Now - ',  Date.now(), ' ', String(now.getDate()) );
+            console.error('Now -- ', now.toLocaleDateString() );
+            console.error('cVencimiento Original - ', value?.cVencimiento );
+            console.error('cVencimiento Convertida -- ', cVencimiento.toLocaleDateString() );
+            console.error('Es menor? ', ( now.getTime() < cVencimiento.getTime() )  );
+            */
+
+            let now = new Date();
+            let nowDay =  now.getDate();
+            let nowMoth =  now.getMonth()+1;
+            let nowYear =  now.getFullYear();
+            let cVencimientoAux = date.transform( value?.cVencimiento, 'DD-MM-YYYY', 'MM/DD/YYYY' );
+            let isGreater = false;
+            let isGreaterThanThreeMonths =  false;
+
+            console.error('Complete object ',  value );
+            console.error('Es fecha valida ', date.isValid( value?.cVencimiento, 'DD-MM-YYYY') );
+            
+            console.error('Now -- ', now.toLocaleDateString() );
+            console.error('Day ', nowDay, ' Month ', nowMoth, ' Year ', nowYear );
+
+            let cVencimiento = new Date( String(cVencimientoAux).trim() );
+            let venDay =  cVencimiento.getDate();
+            let venMoth =  cVencimiento.getMonth()+1;
+            let venYear =  cVencimiento.getFullYear();
+            
+            console.error('cVencimiento Original - ', value?.cVencimiento );
+            console.error('cVencimiento Convertida -- ',  cVencimientoAux );
+            console.error('Day ', venDay , ' Month ', venMoth, ' Year ', venYear );
+
+
+            console.error('Es menor? ', (cVencimiento.getTime() < now.getTime()) );
+            let resMonth = venMoth-nowMoth;
+            console.log('Diferencias de meses ', resMonth );
+
+            if (  cVencimiento.getTime() > now.getTime() ) {
+              console.log('Es mayor ', resMonth );
+              if ( resMonth < 3 ) { // es menor a tres meses
+                isGreater = true;
+              }
+            }
+
+            if ( resMonth > 3 ) { // si vencimiento es mayor a 3 meses
+              isGreaterThanThreeMonths = true;
+              console.error('Es mayor a tres meses ', isGreaterThanThreeMonths );
+            }
+
+
+            this?.ELEMENT_DATA_BE_MADE.push({
+              id: value?.id,
+              nContribution: value?.iNoAport,
+              concept: value?.cConcepto,
+              dueDate: value?.cVencimiento,
+              paymentDate: value?.cFechaPago,
+              amount: value?.deMonto,
+              cTipo: value?.cTipo, 
+              isLess: ( cVencimiento.getTime() < now.getTime()  ),
+              isGreater: isGreater,
+              isGreaterThanThreeMonths: isGreaterThanThreeMonths,
+              isValidDueDate: false
+            });
+  
+            if ( !( now.getTime() < cVencimiento.getTime() ) ) {
+              console.error('OJO ESA ES MAYOR O IGUAL ');
+              // return false;
+            }
+
+          } catch( ex ) {
+            console.error('Error try get Date ', ex );
+          }
+
+          return true;
         });
+
         this.paymentBeMade = this.ELEMENT_DATA_BE_MADE;
-        console.warn( this.paymentBeMade );
+        console.warn('Datos totales de tabla ', this.paymentBeMade );
 
       } else {
 
@@ -87,5 +166,5 @@ export class AppComponent {
     });
 
   }
-  
+
 }
